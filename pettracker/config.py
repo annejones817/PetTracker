3b67bba3fb17962faec9755f8b44d8d61c68f3dd
annejones817 <annejones817@gmail.com>
2017-01-09 16:36:02 -0800

    Updated requirements

--- pettracker/config.py
+++ pettracker/config.py
@@ -1,7 +1,7 @@
 import os
 
 class DevelopmentConfig(object): 
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']#"postgresql://anne:thinkful@localhost:5432/pettracker"
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] #"postgresql://anne:thinkful@localhost:5432/pettracker"
     DEBUG = True
     SECRET_KEY = os.environ.get("PETTRACKER_SECRET_KEY", os.urandom(12))
     UPLOAD_FOLDER = "uploads"
